701ab0ccc21e1aeb64af673ee7da2dd575f664cd
Ilvir Zakiryanov <zakilvir@gmail.com>
2021-10-15 20:18:59 +0500

    version 1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,170 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Requester />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Requester from "./components/Requester.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Requester,
+  },
+};
 </script>
 
 <style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box !important;
+}
+
+.appwrapper {
+  min-height: 100vh;
+  min-width: 100vw;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  font-size: 13px;
+}
+
+.darktheme {
+  background-color: #2c3e50;
+  color: #fff;
+
+  .dialog {
+    background-color: #2c3e50 !important;
+  }
+
+  input,
+  textarea,
+  select {
+    color: #fff;
+    background-color: #333;
+  }
+
+  .answer {
+    background-color: #333;
+    border: 1px solid orange;
+    color: #f5f5f5;
+  }
+}
+
+.container {
+  width: 70vw;
+  margin: 0 auto;
+  padding-bottom: 10px;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.title {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.error_msg {
+  color: red;
+}
+.answer {
+  display: block;
+  padding: 9.5px;
+  margin: 0 0 10px;
+  font-size: 13px;
+  line-height: 1.42857143;
+  color: #333;
+  word-break: break-all;
+  word-wrap: break-word;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  white-space: pre-wrap;
+  overflow-x: auto;
+}
+
+input,
+select,
+textarea {
+  border: 1px solid orange;
+  background-color: #fff;
+  padding: 3px 5px;
+  outline: none;
+  cursor: pointer;
+
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  font-size: 13px;
+}
+
+textarea {
+  min-width: 500px;
+  display: block;
+  margin-bottom: 10px;
+}
+
+select {
+  color: orange;
+}
+
+.go_btn {
+  display: inline-flex;
+  background: orange;
+  padding: 3px 15px;
+  color: white;
+  cursor: pointer;
+  border: 1px solid orange;
+}
+
+.main_set {
+  display: flex;
+  align-items: center;
+  border: 1px solid orange;
+
+  .server_url {
+    color: orange;
+    border-right: 0;
+    padding: 3px 0 3px 5px;
+  }
+  select {
+    border: 0;
+  }
+  input {
+    border: 0;
+    padding-left: 0;
+    width: 500px;
+    flex: 1;
+
+    &.fullBorder {
+      padding-left: 5px;
+    }
+  }
+}
+
+.fields_json {
+  flex: 1;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.field {
+  margin-bottom: 10px;
+  display: flex;
 }
 </style>
---
+++ src/components/Iconics.vue
@@ -0,0 +1,74 @@
+<template>
+  <svg
+    v-if="icon === 'loading'"
+    style="width:30px;height:30px"
+    viewBox="0 0 30 30"
+    class="rotating"
+  >
+    <path fill="currentColor" d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" />
+  </svg>
+
+  <svg
+    v-else-if="icon === 'add'"
+    style="width:24px;height:24px"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z"
+    />
+  </svg>
+
+  <svg
+    v-else-if="icon === 'cancel'"
+    style="width:24px;height:24px"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Iconics",
+  props: {
+    icon: {
+      type: String,
+      default: "",
+    },
+
+    rotating: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+svg {
+  &.rotating {
+    animation: spin 1s linear infinite;
+  }
+}
+
+@-moz-keyframes spin {
+  100% {
+    -moz-transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+@keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+</style>
